a1fad4ee497518a2df65b889a0a120e0cd48b117
Ismail Askhatir <spxoki@gmail.com>
2022-04-16 13:57:07 +0000

    d

--- src/index.js
+++ src/index.js
@@ -38,8 +38,6 @@ function renderTasks() {
     const taskName = document.createElement('div')
     const taskDescrip = document.createElement('div')
     const dueDate = document.createElement('div')
-    const doneBtn = document.createElement('button')
-  //  const removeBtn = document.createElement('button')
     const prioDiv = document.createElement('div')
     div.classList.add('s')
     divTxt.classList.add('txtTask')
@@ -49,12 +47,7 @@ function renderTasks() {
     dueDate.classList.add('duedate')
     prioDiv.classList.add('priotask')
 
-
-
-
-
     Object.keys(task).forEach((prop) => {
-      
     if (prop === 'task') {
       taskName.textContent = task[prop]
     } else if (prop === 'desc') {
@@ -65,15 +58,11 @@ function renderTasks() {
       prioDiv.textContent = `Priority:` + ' ' + task[prop]
     }
     })
-// taskName.textContent = task.task
-// taskDescrip.textContent = task.desc
-// dueDate.textContent = task.date
-// prioDiv.textContent = task.priority
+
     if (task.done) {
       div.classList.add('newone')
     } else if (!task.done) {
       div.classList.add('s')
-
     }
 
     container.append(div)
@@ -91,41 +80,27 @@ function renderTasks() {
     exitForm()    
 }
 
-
 function createDelete(index) {
 const removeBtn = document.createElement('button')
 removeBtn.textContent = 'Remove'
   removeBtn.addEventListener('click', () => {
     myTasks.splice(index, 1)
-    
-   
 renderTasks()
-
   })
   return removeBtn
 }
+
 function doneEffect(task, had) {
   const doneBtn = document.createElement('button')
   doneBtn.textContent = 'Done'
   doneBtn.addEventListener('click', () => {
     task.done = true;
     had.classList.add('newone')
-
     renderTasks()
-    
   })
   return doneBtn
 }
 
-
-// function createDelete(index, arr) {
-//   const removeBtn = document.createElement('button')
-//   removeBtn.textContent = 'Remove'
-//   removeBtn.addEventListener('click', () => {
-//       arr.splice(index, 1)
-//   })
- //  return removeBtn
-// } 
   addProject.addEventListener('click', () => {
     let newArr =  inputProject.value
     let div = document.createElement('div')
@@ -138,31 +113,27 @@ function doneEffect(task, had) {
     return newArr
   })
 
-
 function hidePro() {
   inputProject.style.visibility = 'hidden'
   addProject.style.visibility = 'hidden'
-
 }
 
 function showPro() {
   inputProject.style.visibility = 'visible'
   addProject.style.visibility = 'visible'
 }
+
+function exitForm() {
+  formShow.style.visibility = "hidden"
+}
+function showForm() {
+  formShow.style.visibility = "visible"
+}
+
 showProject.addEventListener('click', showPro)
 btnExit.addEventListener('click', exitForm)
 addTask.addEventListener('click',addtask)
 showBtn.addEventListener('click', showForm)
-
-function exitForm() {
-    formShow.style.visibility = "hidden"
-}
-function showForm() {
-    formShow.style.visibility = "visible"
-
-}
-
-
 
 
 function Task(task, description, date, priority) {
@@ -171,12 +142,9 @@ function Task(task, description, date, priority) {
     this.date = date,
     this.priority = priority,
     this.done = false
-    
 }
 
 function addtask() {
-
-
     let taskpara = taskInput.value
     let descriptionpara = descInput.value
     let datee = inputDate.value
@@ -188,7 +156,6 @@ taskInput.value = ''
 descInput.value = ''
 
    renderTasks()
-   console.log(myTasks)
 }
 
 const localLeads = JSON.parse(localStorage.getItem("myTasks"))
