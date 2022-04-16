dfceecb0269c961c4f58dc75129e6569a3448af0
Ismail Askhatir <spxoki@gmail.com>
2022-04-16 13:17:09 +0000

    d

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ const descInput = document.querySelector('#taskDesc')
 const container = document.getElementById('taskdiv')
 const inputDate = document.querySelector('#appt')
 const prioInput = document.getElementById('priority')
+const showProject = document.querySelector('.btnthis')
+const inputProject = document.querySelector('#projectName')
+const addProject = document.querySelector('.addProject')
 
 
 
@@ -66,6 +69,12 @@ function renderTasks() {
 // taskDescrip.textContent = task.desc
 // dueDate.textContent = task.date
 // prioDiv.textContent = task.priority
+    if (task.done) {
+      div.classList.add('newone')
+    } else if (!task.done) {
+      div.classList.add('s')
+
+    }
 
     container.append(div)
     div.append(divTxt)
@@ -74,28 +83,22 @@ function renderTasks() {
     divTxt.append(taskDescrip)
     divTxt.append(dueDate)
     divTxt.append(prioDiv)
-
-  
     divBtn.appendChild(doneEffect(task, div))
-  //  removeBtn.textContent = 'Remove'
-    // divBtn.append(removeBtn)
-
     divBtn.append(createDelete(index))
    })
    taskInput.value = ''
    descInput.value = ''
-    exitForm()
-
-
-    
+    exitForm()    
 }
+
 
 function createDelete(index) {
 const removeBtn = document.createElement('button')
 removeBtn.textContent = 'Remove'
-  removeBtn.addEventListener('click', (e) => {
+  removeBtn.addEventListener('click', () => {
     myTasks.splice(index, 1)
-
+    
+   
 renderTasks()
 
   })
@@ -107,10 +110,13 @@ function doneEffect(task, had) {
   doneBtn.addEventListener('click', () => {
     task.done = true;
     had.classList.add('newone')
+
+    renderTasks()
     
   })
   return doneBtn
 }
+
 
 // function createDelete(index, arr) {
 //   const removeBtn = document.createElement('button')
@@ -120,7 +126,30 @@ function doneEffect(task, had) {
 //   })
  //  return removeBtn
 // } 
+  addProject.addEventListener('click', () => {
+    let newArr =  inputProject.value
+    let div = document.createElement('div')
+    div.classList.add('divnav')
+    div.textContent = newArr
+    nav.appendChild(div)
+    newArr = new Array;
+    console.log(newArr)
+    hidePro()
+    return newArr
+  })
 
+
+function hidePro() {
+  inputProject.style.visibility = 'hidden'
+  addProject.style.visibility = 'hidden'
+
+}
+
+function showPro() {
+  inputProject.style.visibility = 'visible'
+  addProject.style.visibility = 'visible'
+}
+showProject.addEventListener('click', showPro)
 btnExit.addEventListener('click', exitForm)
 addTask.addEventListener('click',addtask)
 showBtn.addEventListener('click', showForm)
@@ -154,11 +183,17 @@ function addtask() {
     let priority = prioInput.value
     let newTask = new Task(taskpara, descriptionpara, datee, priority)
     myTasks.push(newTask)
-
+    localStorage.setItem("myTasks", JSON.stringify(myTasks))
 taskInput.value = ''
 descInput.value = ''
 
    renderTasks()
    console.log(myTasks)
 }
+
+const localLeads = JSON.parse(localStorage.getItem("myTasks"))
+    if (localLeads) {
+        myTasks = localLeads
+        renderTasks()
+    }
 
